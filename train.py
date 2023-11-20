fe1b6a1a045fd9491a6a5ed4083f97532419cad3
nulls8 <moon-hotel@hotmail.com>
2023-11-20 20:40:11 +0800

    优化代码添加注释

--- train.py
+++ train.py
@@ -36,11 +36,11 @@ def load_raw_data(file_dir="./data/peotry_tang"):
             for item in data:
                 content = item['paragraphs']  # ['日滿東窗照被堆，宿湯猶自暖如煨。', '尺三汗脚君休笑，曾踏鞾霜待漏來。']
                 content = "".join(content)  # '日滿東窗照被堆，宿湯猶自暖如煨。尺三汗脚君休笑，曾踏鞾霜待漏來。'
-                if len(content) < 10 :  # 太短的诗过滤
+                if len(content) < 10:  # 太短的诗过滤
                     continue
                 if '《' in content or '（' in content or '□' in content or '[' in content:  #
                     continue
-                samples.append(content)  # ['','日滿東窗照被堆，宿湯猶自暖如煨。', '尺三汗脚君休笑，曾踏鞾霜待漏來。']
+                samples.append(content)  # ['日滿東窗照被堆，宿湯猶自暖如煨。尺三汗脚君休笑，曾踏鞾霜待漏來。', ...]
         return samples
 
     all_samples = []
@@ -78,50 +78,57 @@ class Net(pl.LightningModule):
         self.dataset_valid = DS(self.data[-valid_examples:], vocab_path=vocab_path, max_length=max_length)
 
     def forward(self, input_ids, attention_mask):
-        input_ids = input_ids
-        attention_mask = attention_mask
-        r = self.model(input_ids=input_ids,
-                       attention_mask=attention_mask,
-                       labels=input_ids,
-                       return_dict=True)
+        r = self.model(input_ids=input_ids, attention_mask=attention_mask,
+                       labels=input_ids, return_dict=True)
         return r["loss"]
 
     def train_dataloader(self):
-        return DataLoader(self.dataset_train,
-                          batch_size=self.batch_size,
-                          num_workers=8,
-                          shuffle=True,
-                          drop_last=True)
+        """
+        定义用于训练的迭代器
+        """
+        return DataLoader(self.dataset_train, batch_size=self.batch_size,
+                          num_workers=8, shuffle=True, drop_last=True)
 
     def val_dataloader(self):
-        return DataLoader(self.dataset_valid,
-                          batch_size=self.batch_size,
-                          num_workers=8,
-                          shuffle=True,
-                          drop_last=True)
+        """
+        定义用于验证的迭代器
+        """
+        return DataLoader(self.dataset_valid, batch_size=self.batch_size,
+                          num_workers=8, shuffle=True, drop_last=True)
 
     def configure_optimizers(self):
+        """
+        定义优化器，和学习率调度器（这个也可以不用）
+        """
         optimizer = AdamW(self.parameters(), lr=self.lr, weight_decay=0.001)
         scheduler = get_linear_schedule_with_warmup(optimizer, self.warm_up_steps, self.t_total)
         scheduler = {"scheduler": scheduler, "interval": "step", "frequency": 1}
         return [optimizer], [scheduler]
 
     def training_step(self, batch, batch_nb):
+        """
+        定义训练步骤，返回损失
+        """
         loss = self.forward(batch["input_ids"], batch["attention_mask"])
         self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         return loss
 
     def validation_step(self, batch, batch_nb):
+        """
+        定义验证步骤，计算验证集上每个batch的损失并放到一个list中，后续validation_epoch_end()会使用到
+        """
         loss = self.forward(batch["input_ids"], batch["attention_mask"])
         return loss
 
     def validation_epoch_end(self, outputs):
         avg_loss = torch.stack(outputs).mean()
+        # 计算整个验证集山的平均损失，outputs为validation_step()中计算完成的每个batch损失
         self.log("val_loss", avg_loss, on_epoch=True, prog_bar=True, logger=True)
+        # 添加日志记录
         return {"val_loss": avg_loss}
 
 
-if __name__ == "__main__":
+if __name__ == "__main__s":
     parser = argparse.ArgumentParser()
     parser.add_argument("--device", default="0", type=str, required=False, help="设置使用哪些显卡，用逗号分割")
     parser.add_argument("--config_path", default="model/config.json", type=str, required=False,
@@ -132,9 +139,9 @@ if __name__ == "__main__":
     parser.add_argument("--batch_size", default=2, type=int, required=False, help="训练batch size")
     parser.add_argument("--lr", default=1.5e-4, type=float, required=False, help="学习率")
     parser.add_argument("--warmup_steps", default=2000, type=int, required=False, help="warm up步数")
-    parser.add_argument("--max_length", default=1024, type=int, required=False, help="单条文本最长长度")
-    parser.add_argument("--eval_interval", default=100, type=int, required=False, help="eval 步数")
-    parser.add_argument("--val_examples", default=100, type=int, required=False, help="选择多少验证集样本")
+    parser.add_argument("--max_length", default=256, type=int, required=False, help="单条文本最长长度")
+    parser.add_argument("--eval_interval", default=100, type=int, required=False, help="多少batch在验证集上验证一次")
+    parser.add_argument("--val_examples", default=1000, type=int, required=False, help="选择多少验证集样本进行验证")
     parser.add_argument("--t_total", default=100000, type=int, required=False, help="计划训练多少步")
     parser.add_argument("--log_step", default=1, type=int, required=False, help="多少步汇报一次loss")
     parser.add_argument("--output_dir", default="model/", type=str, required=False, help="模型输出路径")
@@ -152,31 +159,20 @@ if __name__ == "__main__":
     config_path = args.config_path
     t_total = args.t_total
 
-    checkpoint_callback = ModelCheckpoint(dirpath=output_path,
-                                          verbose=True,
-                                          period=1,
-                                          save_top_k=1,
-                                          monitor="val_loss",
-                                          mode="min")
+    checkpoint_callback = ModelCheckpoint(dirpath=output_path, verbose=True,
+                                          period=1, save_top_k=1,
+                                          monitor="val_loss", mode="min")
+    # 间隔1个epoch，按val_loss最小值保存1个模型
     learning_rate_callback = LearningRateMonitor()
-    trainer = pl.Trainer(default_root_dir=output_path,
-                         gradient_clip_val=1,
-                         max_epochs=epochs,
-                         gpus=args.device,
-                         distributed_backend="dp",
-                         val_check_interval=eval_interval,
+    trainer = pl.Trainer(default_root_dir=output_path, gradient_clip_val=1,
+                         max_epochs=epochs, gpus=args.device,
+                         distributed_backend="dp", val_check_interval=eval_interval,
                          callbacks=[learning_rate_callback, checkpoint_callback],
                          precision=32)
-    net = Net(batch_size,
-              epochs,
-              t_total=t_total,
-              config_path=config_path,
-              data_path=data_path,
-              valid_examples=val_examples,
-              vocab_path=vocab_path,
-              max_length=max_length,
-              warm_up_steps=warmup_steps,
-              lr=lr)
+    net = Net(batch_size, epochs, t_total=t_total,
+              config_path=config_path, data_path=data_path,
+              valid_examples=val_examples, vocab_path=vocab_path,
+              max_length=max_length, warm_up_steps=warmup_steps, lr=lr)
     # d = torch.load('output_old/best.ckpt', map_location=torch.device("cpu"))["state_dict"]
     # d.pop('model.classifier.bias')
     # d.pop('model.classifier.weight')
